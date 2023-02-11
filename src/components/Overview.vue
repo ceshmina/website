2c092dab8d4ad26b7626f8b4dd7baee779df2e0a
ceshmina <shu@ceshmina.com>
2023-02-11 13:26:03 +0900

    デフォルト生成された内容を削除し、アイコンとタイトルを追加

---
+++ src/components/Overview.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="overview">
+    <div class="overview-icon">
+      <img src="/icon_circle.png" class="overview-icon-img" />
+    </div>
+    <div class="overview-text">
+      <h1 class="overview-text-name">shu (@ceshmina)</h1>
+      <p class="overview-text-job">data scientist | musician</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.overview-icon-img {
+  width: 180px;
+}
+.overview-text-name {
+  font-size: 32px;
+  margin: 32px 0 8px;
+}
+.overview-text-job {
+  font-size: 24px;
+  margin: 8px 0 32px;
+}
+</style>
