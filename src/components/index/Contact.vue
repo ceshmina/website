5a48dc9da494324950c8945a0ba29c3a2f99a839
ceshmina <shu@ceshmina.com>
2023-02-24 22:33:05 +0900

    スタイル調整

--- src/components/index/Contact.vue
+++ src/components/index/Contact.vue
@@ -10,9 +10,15 @@ const github = mdiGithub
   <div class="contact">
     <h1 class="contact-title">contact</h1>
     <div class="sns-icons">
-      <a href="https://twitter.com/ceshmina" target="_blank"><v-icon class="sns-icon">{{ twitter }}</v-icon></a>
-      <a href="https://instagram.com/ceshmina" target="_blank"><v-icon class="sns-icon">{{ instagram }}</v-icon></a>
-      <a href="https://github.com/ceshmina" target="_blank"><v-icon class="sns-icon">{{ github }}</v-icon></a>
+      <a href="https://twitter.com/ceshmina" target="_blank">
+        <v-btn icon variant="text" class="sns-icon"><v-icon class="test">{{ twitter }}</v-icon></v-btn>
+      </a>
+      <a href="https://instagram.com/ceshmina" target="_blank">
+        <v-btn icon variant="text" class="sns-icon"><v-icon>{{ instagram }}</v-icon></v-btn>
+      </a>
+      <a href="https://github.com/ceshmina" target="_blank">
+        <v-btn icon variant="text" class="sns-icon"><v-icon>{{ github }}</v-icon></v-btn>
+      </a>
     </div>
   </div>
 </template>
@@ -27,10 +33,8 @@ const github = mdiGithub
 .sns-icons
   margin: 0 auto
 
-  a
-    color: #213547
-
   .sns-icon
-    font-size: 32px
+    font-size: 24px
     margin: 0 16px
+    color: #213547
 </style>
