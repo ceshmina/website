80349a8ebb11e0b29113dae1538b3b250d27f26f
ceshmina <shu@ceshmina.com>
2023-03-05 12:51:05 +0900

    AxiosでAPIサーバーから日記を取得

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,23 @@
 
 <template>
   <router-view />
+
+  <nav class="nav">
+    <ul class="nav-list">
+      <li><router-link to="/">home</router-link></li>
+      <li><router-link to="/diary">diary</router-link></li>
+    </ul>
+  </nav>
 </template>
 
-<style scoped>
+<style scoped lang="sass">
+.nav-list
+  display: flex
+  width: 100px
+  justify-content: space-between
+  padding-left: 0
+  margin: 0 auto 80px
+
+  li
+    list-style: none
 </style>
--- src/pages/Diary.vue
+++ src/pages/Diary.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import axios from 'axios'
+import { ref } from 'vue'
+
+console.log('hoge')
+
+const data = ref(null)
+const api_host = import.meta.env.VITE_API_HOST
+
+const getData = async() => {
+    axios.get(`${api_host}/diary/202301`).then(res => data.value = res.data)
+}
+getData()
 </script>
 
 <template>
   <h1>Diary</h1>
-  <p>Under preparation</p>
+  <p>{{ data }}</p>
 </template>
 
 <style scoped lang="sass">
