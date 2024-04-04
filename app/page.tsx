9ec73ca07e5b3272ed3a510c4309328d13d54abf
ceshmina <shu@ceshmina.com>
2024-04-04 22:55:24 +0900

    自己紹介を追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 const Home = () => {
   return (
-    <main>
-      <h1>ceshmina</h1>
+    <main className="max-w-[800px] mx-auto p-4">
+      <section className="py-4">
+        <h1 className="text-2xl font-bold">ceshmina</h1>
+        <p className="py-1">shu's website</p>
+      </section>
+
+      <section className="py-4">
+        <h2 className="text-lg font-bold">About</h2>
+        <div className="py-2 text-sm">
+          <p className="py-1">ソフトウェアエンジニア、データサイエンティスト見習い。</p>
+          <p className="py-1">南米の民族風音楽「フォルクローレ」を演奏します。YAMA、グルーポ★ブラザーズ、Los Amidasというグループで活動しています。</p>
+        </div>
+      </section>
     </main>
   )
 }
