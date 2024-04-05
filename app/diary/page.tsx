24ed9f9e9d549d77c7aa69aa8ddb29f3e9b89fe3
ceshmina <shu@ceshmina.com>
2024-04-05 21:24:47 +0900

    指定したディレクトリから日記一覧を取得し、そのまま表示

---
+++ app/diary/page.tsx
@@ -0,0 +1,48 @@
+import { promises as fs } from 'fs'
+
+type Diary = {
+  slug: string
+  content: string
+}
+
+const getMarkdownFiles = async (dir: string) => {
+  const entries = await fs.readdir(dir, { withFileTypes: true })
+  const res: Diary[] = []
+  for (const entry of entries) {
+    if (entry.isDirectory()) {
+      const res2 = await getMarkdownFiles(`${dir}/${entry.name}`)
+      res.push(...res2)
+    } else if (entry.name.endsWith('.md')) {
+      const content = await fs.readFile(`${dir}/${entry.name}`, 'utf-8')
+      res.push({
+        slug: entry.name.replace(/\.md$/, ''),
+        content
+      })
+    }
+  }
+  return res
+}
+
+const Page = async () => {
+  const diaries = await getMarkdownFiles('data/diary')
+  return (
+    <main className="max-w-[800px] mx-auto p-4">
+      <section className="py-4">
+        <h1 className="text-2xl font-bold">diary</h1>
+      </section>
+
+      <section className="py-4">
+        {diaries.map(diary => (
+          <div key={diary.slug} className="py-2">
+            <h2 className="text-lg font-bold">{diary.slug}</h2>
+            <div className="py-2 text-sm">
+              {diary.content}
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
+  )
+}
+
+export default Page
