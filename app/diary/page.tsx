b1e04a0a54e5031ede906ff85b6094be7ca1abc0
ceshmina <shu@ceshmina.com>
2024-04-30 22:28:21 +0900

    撮影機材別へのリンクをサイドバーへ

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -14,20 +14,26 @@ const Page = async () => {
           <p><Link href="/" className="text-blue-500">戻る</Link></p>
         </div>
         <h1 className="text-2xl font-bold">diary</h1>
-        <p className="my-3 text-xs text-gray-500">
-          {cameras.map(({ camera, count }) =>
-            <span key={camera.slug} className="inline-block mr-2 my-1 border-2 border-gray-300 px-1 py-0.5 rounded">
-              <Link href={`/diary/camera/${camera.slug}`} className="text-blue-500">{camera.name} ({count})</Link>
-            </span>
-          )}
-        </p>
       </section>
 
-      <section className="py-4">
-        {diaries.sorted.map(diary => (
-          <Card key={diary.slug} diary={diary} />
-        ))}
-      </section>
+      <div className="md:flex py-4">
+        <section className="md:w-[70%]">
+          {diaries.sorted.map(diary => (
+            <Card key={diary.slug} diary={diary} />
+          ))}
+        </section>
+        
+        <div className="md:w-[30%] md:pl-4 py-4">
+          <h2 className="text-sm font-bold">撮影機材別</h2>
+          <p className="my-4 text-xs text-gray-500">
+            {cameras.map(({ camera, count }) =>
+              <span key={camera.slug} className="inline-block mr-2 my-1 border-2 border-gray-300 px-1 py-0.5 rounded">
+                <Link href={`/diary/camera/${camera.slug}`} className="text-blue-500">{camera.name} ({count})</Link>
+              </span>
+            )}
+          </p>
+        </div>
+      </div>
     </main>
   )
 }
