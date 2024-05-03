04c754440225fe492fa4351006c62cbee01af451
ceshmina <shu@ceshmina.com>
2024-05-03 12:25:14 +0900

    サイドバーのデザイン調整

--- components/diary/sidebar.tsx
+++ components/diary/sidebar.tsx
@@ -11,15 +11,15 @@ const Sidebar = (props: Props) => {
   return (
     <div>
       <h2 className="text-sm font-bold">月別</h2>
-      <p className="my-4 text-xs text-gray-500">
+      <div className="my-4 text-sm text-gray-500">
         {months.map(({ month, count }) =>
-          <span key={month.slug} className="inline-block mr-2 my-1 border-2 border-gray-300 px-1 py-0.5 rounded">
+          <p key={month.slug} className="my-1.5">
             <Link href={`/diary/month/${month.slug}`} className="text-blue-500">{month.name} ({count})</Link>
-          </span>
+          </p>
         )}
-      </p>
+      </div>
 
-      <h2 className="text-sm font-bold">撮影機材別</h2>
+      <h2 className="mt-8 text-sm font-bold">撮影機材別</h2>
       <p className="my-4 text-xs text-gray-500">
         {cameras.map(({ camera, count }) =>
           <span key={camera.slug} className="inline-block mr-2 my-1 border-2 border-gray-300 px-1 py-0.5 rounded">
