af3eb50b9f9c0fe6306f60bfce88b9f6a16a0bcf
ceshmina <shu@ceshmina.com>
2024-05-04 11:51:30 +0900

    ページネーションのナビゲーション作成

--- app/diary/list/[page]/page.tsx
+++ app/diary/list/[page]/page.tsx
@@ -17,7 +17,11 @@ const Page = async ({ params }: { params: { page: string }}) => {
   const { page } = params
 
   const diariesAll = await getDiaries('data/diary')
-  const diaries = new DiaryCollection(diariesAll.sorted.slice((parseInt(page) - 1) * perPage, parseInt(page) * perPage))
+  const n = diariesAll.items.length
+  const numPages = Math.ceil(n / perPage)
+  const minIndex = (parseInt(page) - 1) * perPage
+  const maxIndex = Math.min(n, parseInt(page) * perPage)
+  const diaries = new DiaryCollection(diariesAll.sorted.slice(minIndex, maxIndex))
 
   const months = aggByMonth(diariesAll.items)
   const cameras = await aggCameras(diariesAll.items)
@@ -32,11 +36,28 @@ const Page = async ({ params }: { params: { page: string }}) => {
       </section>
 
       <div className="md:flex py-4">
-        <section className="md:w-[70%]">
-          {diaries.sorted.map(diary => (
-            <Card key={diary.slug} diary={diary} />
-          ))}
-        </section>
+        <div className="md:w-[70%]">
+          <section>
+            {diaries.sorted.map(diary => (
+              <Card key={diary.slug} diary={diary} />
+            ))}
+          </section>
+
+          <section className="py-4 text-center text-sm text-gray-500">
+            <p className="py-1">
+              {minIndex + 1} - {maxIndex} / {n}件
+            </p>
+            <p className="py-1">
+              <span className="mr-2 border-2 px-1.5 py-0.5 rounded-full">{parseInt(page) > 1
+                ? <Link href={`/diary/list/${parseInt(page) - 1}`} className="text-blue-500">&lt;</Link>
+                : <>&lt;</>}</span>
+              {` ${page} / ${numPages} `}
+              <span className="ml-2 border-2 px-1.5 py-0.5 rounded-full">{parseInt(page) < Math.ceil(n / perPage)
+                ? <Link href={`/diary/list/${parseInt(page) + 1}`} className="text-blue-500">&gt;</Link>
+                : <>&gt;</>}</span>
+            </p>
+          </section>
+        </div>
 
         <div className="md:w-[30%] md:pl-4 py-4">
           <Sidebar months={months} cameras={cameras} />
