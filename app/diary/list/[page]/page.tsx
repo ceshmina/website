2359ef1cbf325262e184a25665001d662cb30de1
ceshmina <shu@ceshmina.com>
2024-05-04 15:43:37 +0900

    ページネーションをコンポーネント化

--- app/diary/list/[page]/page.tsx
+++ app/diary/list/[page]/page.tsx
@@ -1,28 +1,28 @@
 import Link from 'next/link'
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
 import { DiaryCollection } from '@/core/diary/model'
 import { aggByMonth, aggCameras } from '@/core/diary/aggregate'
 import { getDiaries } from '@/core/diary/retrieve'
 import Card from '@/components/diary/card'
 import Sidebar from '@/components/diary/sidebar'
+import { Paginator } from '@/core/diary/pagination'
+import Pagination from '@/components/diary/pagination'
 
-const perPage = 25
+const PER_PAGE = 25
 
 export const generateStaticParams = async () => {
   const diaries = await getDiaries('data/diary')
-  const numPages = Math.ceil(diaries.items.length / perPage)
-  return Array.from({ length: numPages }, (_, i) => ({ page: (i + 1).toString() }))
+  const paginator = new Paginator(diaries.items.length, PER_PAGE)
+  return Array.from({ length: paginator.numPages() }, (_, i) => ({ page: (i + 1).toString() }))
 }
 
 const Page = async ({ params }: { params: { page: string }}) => {
   const { page } = params
+  const pageInt = parseInt(page)
 
   const diariesAll = await getDiaries('data/diary')
   const n = diariesAll.items.length
-  const numPages = Math.ceil(n / perPage)
-  const minIndex = (parseInt(page) - 1) * perPage
-  const maxIndex = Math.min(n, parseInt(page) * perPage)
-  const diaries = new DiaryCollection(diariesAll.sorted.slice(minIndex, maxIndex))
+  const paginator = new Paginator(n, PER_PAGE)
+  const diaries = new DiaryCollection(diariesAll.sorted.slice(paginator.minIndex(pageInt), paginator.maxIndex(pageInt)))
 
   const months = aggByMonth(diariesAll.items)
   const cameras = await aggCameras(diariesAll.items)
@@ -45,26 +45,7 @@ const Page = async ({ params }: { params: { page: string }}) => {
           </section>
 
           <section className="py-4 text-center text-sm text-gray-500">
-            <p className="py-1">
-              {minIndex + 1} - {maxIndex} / {n}件
-            </p>
-            <p className="py-1">
-              {parseInt(page) > 1
-                ? <span className="mr-2 border-2 border-gray-300 px-1 py-1 rounded-full">
-                    <Link href={`/diary/list/${parseInt(page) - 1}`} className="text-blue-500"><ChevronLeftIcon className="w-4 h-4 inline-block pb-0.5" /></Link>
-                  </span>
-                : <span className="mr-2 border-2 border-gray-200 px-1 py-1 rounded-full text-gray-300">
-                    <ChevronLeftIcon className="w-4 h-4 inline-block pb-0.5" />
-                  </span>}
-              {` ${page} / ${numPages} `}
-              {parseInt(page) < Math.ceil(n / perPage)
-                ? <span className="ml-2 border-2 border-gray-300 px-1 py-1 rounded-full">
-                    <Link href={`/diary/list/${parseInt(page) + 1}`} className="text-blue-500"><ChevronRightIcon className="w-4 h-4 inline-block pb-0.5" /></Link>
-                  </span>
-                : <span className="ml-2 border-2 border-gray-200 px-1 py-1 rounded-full text-gray-300">
-                    <ChevronRightIcon className="w-4 h-4 inline-block pb-0.5" />
-                  </span>}
-            </p>
+            <Pagination paginator={paginator} page={pageInt} />
           </section>
         </div>
 
---
+++ components/diary/pagination.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
+import { Paginator } from '@/core/diary/pagination'
+
+const Pagination = (props: { paginator: Paginator, page: number }) => {
+  const { paginator, page } = props
+  const n = paginator.n
+  const perPage = paginator.perPage
+  const numPages = paginator.numPages()
+  const minIndex = paginator.minIndex(page)
+  const maxIndex = paginator.maxIndex(page)
+
+  return (
+    <div>
+      <p className="py-1">
+        {minIndex + 1} - {maxIndex} / {n}件
+      </p>
+      <p className="py-1">
+        {page > 1
+          ? <span className="mr-2 border-2 border-gray-300 px-1 py-1 rounded-full">
+              <Link href={`/diary/list/${page - 1}`} className="text-blue-500"><ChevronLeftIcon className="w-4 h-4 inline-block pb-0.5" /></Link>
+            </span>
+          : <span className="mr-2 border-2 border-gray-200 px-1 py-1 rounded-full text-gray-300">
+              <ChevronLeftIcon className="w-4 h-4 inline-block pb-0.5" />
+            </span>}
+        {` ${page} / ${numPages} `}
+        {page < Math.ceil(n / perPage)
+          ? <span className="ml-2 border-2 border-gray-300 px-1 py-1 rounded-full">
+              <Link href={`/diary/list/${page + 1}`} className="text-blue-500"><ChevronRightIcon className="w-4 h-4 inline-block pb-0.5" /></Link>
+            </span>
+          : <span className="ml-2 border-2 border-gray-200 px-1 py-1 rounded-full text-gray-300">
+              <ChevronRightIcon className="w-4 h-4 inline-block pb-0.5" />
+            </span>}
+      </p>
+    </div>
+  )
+}
+
+export default Pagination
