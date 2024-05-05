7b4bb5c811fda3175cc86dc7f1c0f862578df7c3
ceshmina <shu@ceshmina.com>
2024-05-05 22:32:38 +0900

    フォント統一

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
+import { Bitter, Noto_Sans_JP } from 'next/font/google'
 import './globals.css'
 
-const mainFont = Open_Sans({ subsets: ['latin'], weight: ["300"] })
+const mainFont = Bitter({ subsets: ['latin'], weight: ["300"], variable: '--font-main' })
+const mainJPFont = Noto_Sans_JP({ subsets: ['latin'], weight: ["300"], variable: '--font-jp' })
 
 export const metadata: Metadata = {
   title: 'ceshmina'
@@ -11,7 +12,7 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <html lang="en">
-      <body className={mainFont.className}>{children}</body>
+      <body className={`${mainFont.variable} ${mainJPFont.variable}`}>{children}</body>
       <footer className="my-8">
         <p className={`${mainFont.className} text-center text-[10px] font-light text-gray-500`}>&copy;2024 APKAS</p>
       </footer>
