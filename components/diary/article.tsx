94eb43047074d1ee89d46dcb14d4a8b0fee90f51
ceshmina <shu@ceshmina.com>
2024-05-13 21:04:01 +0900

    記事内のリンクに対応

--- components/diary/article.tsx
+++ components/diary/article.tsx
@@ -6,6 +6,11 @@ const Article = (props: { content: string }) => {
   return (
     <Markdown components={{
       p: ({ children }) => <p className="py-1">{children}</p>,
+      a: ({ children, href }) => (
+        href && href.startsWith('http')
+        ? <a href={href} className="text-blue-600" target="_blank">{children}</a>
+        : <a href={href} className="text-blue-600">{children}</a>
+      ),
       img: ({ src }) => <div className="py-4"><Image src={src || null} /></div>,
       hr: () => <hr className="mx-auto my-8 w-[300px] max-w-[50%] h-[3px] bg-gray-200 border-0 rounded" />,
     }} >{content}</Markdown>
