05863ee69caf58b995033c113e7c839801807d66
ceshmina <shu@ceshmina.com>
2024-06-01 10:47:03 +0900

    イベント履歴に場所を追加

--- app/page.tsx
+++ app/page.tsx
@@ -8,28 +8,28 @@ const discography = [
 ]
 
 const performances = [
-  { date: '20230219', title: '冬のアルスコンサート', note: 'as Grupo Brothers' },
-  { date: '20230313', title: '筑波大学フォルクローレ愛好会定期コンサート', note: 'as Grupo Brothers' },
-  { date: '20230409', title: 'アンデスの家ボリビア インストアライブ', note: 'as YAMA' },
-  { date: '20230503', title: 'フォルクローレ演奏会2023 en IRIFUNE', note: 'as YAMA' },
-  { date: '20230618', title: '', note: 'as YAMA' },
-  { date: '20230805', title: 'デイケアなんかい コンサート', note: 'as YAMA' },
-  { date: '20230805', title: 'MGユースホステル コンサート', note: 'as YAMA' },
-  { date: '20230806', title: '原爆ピアノ アオギリ平和コンサート', note: 'as YAMA' },
-  { date: '20230806', title: 'きっちんたまがわ コンサート', note: 'as YAMA' },
-  { date: '20230811', title: 'COSQUIN en JAPON 2023', note: 'as Grupo Brothers' },
-  { date: '20230812', title: 'COSQUIN en JAPON 2023 (サブステージ)', note: 'as YAMA' },
-  { date: '20231028', title: 'フォルク人達', note: 'as YAMA/with アシタバ' },
-  { date: '20231104', title: 'マチュピチュ・エン・ハポン2023', note: 'as YAMA' },
-  { date: '20231105', title: 'アンデスの響きコンサート', note: 'as YAMA/with MAYA, Raul Olarte' },
-  { date: '20231125', title: '三田祭', note: 'support' },
-  { date: '20240217', title: '冬のアルスコンサート', note: 'as Grupo Brothers' },
-  { date: '20240225', title: 'ワールドミュージックフェスタ', note: 'as Grupo Brothers' },
-  { date: '20240421', title: 'ホセ犬伏『再会のサンバ』リリース記念フォルクローレコンサート', note: 'support' },
-  { date: '20240428', title: 'フォルクローレ演奏会2024 en IRIFUNE', note: 'as YAMA' },
-  { date: '20240506', title: '水道橋フォルクローレライブ春', note: 'as Grupo Brothers/with tapaTunes' },
-  { date: '20240519', title: '第27回三木山フォルクローレ音楽祭' },
-  { date: '20240525', title: '', note: 'as Los Amidas/with ショルヘーノ, すずきかのん, 多賀文音' }
+  { date: '20230219', title: '冬のアルスコンサート', location: 'Tsukuba', note: 'as Grupo Brothers' },
+  { date: '20230313', title: '筑波大学フォルクローレ愛好会定期コンサート', location: 'Tsukuba', note: 'as Grupo Brothers' },
+  { date: '20230409', title: 'アンデスの家ボリビア インストアライブ', location: 'Kodaira', note: 'as YAMA' },
+  { date: '20230503', title: 'フォルクローレ演奏会2023 en IRIFUNE', location: 'Yokohama', note: 'as YAMA' },
+  { date: '20230618', title: '', location: 'Hachioji', note: 'as YAMA' },
+  { date: '20230805', title: 'デイケアなんかい コンサート', location: 'Hiroshima', note: 'as YAMA' },
+  { date: '20230805', title: 'MGユースホステル コンサート', location: 'Hiroshima', note: 'as YAMA' },
+  { date: '20230806', title: '原爆ピアノ アオギリ平和コンサート', location: 'Hiroshima', note: 'as YAMA' },
+  { date: '20230806', title: 'きっちんたまがわ コンサート', location: 'Hiroshima', note: 'as YAMA' },
+  { date: '20230811', title: 'COSQUIN en JAPON 2023', location: 'Kawamata', note: 'as Grupo Brothers' },
+  { date: '20230812', title: 'COSQUIN en JAPON 2023 (サブステージ)', location: 'Kawamata', note: 'as YAMA' },
+  { date: '20231028', title: 'フォルク人達', location: 'Kunitachi', note: 'as YAMA/with アシタバ' },
+  { date: '20231104', title: 'マチュピチュ・エン・ハポン2023', location: 'Iwakuni', note: 'as YAMA' },
+  { date: '20231105', title: 'アンデスの響きコンサート', location: 'Yamaguchi', note: 'as YAMA/with MAYA, Raul Olarte' },
+  { date: '20231125', title: '三田祭', location: 'Yokohama', note: 'support' },
+  { date: '20240217', title: '冬のアルスコンサート', location: 'Tsukuba', note: 'as Grupo Brothers' },
+  { date: '20240225', title: 'ワールドミュージックフェスタ', location: 'Yokohama', note: 'as Grupo Brothers' },
+  { date: '20240421', title: 'ホセ犬伏『再会のサンバ』リリース記念フォルクローレコンサート', location: 'Tachikawa', note: 'support' },
+  { date: '20240428', title: 'フォルクローレ演奏会2024 en IRIFUNE', location: 'Yokohama', note: 'as YAMA' },
+  { date: '20240506', title: '水道橋フォルクローレライブ春', location: 'Tokyo', note: 'as Grupo Brothers/with tapaTunes' },
+  { date: '20240519', title: '第27回三木山フォルクローレ音楽祭', location: 'Miki' },
+  { date: '20240525', title: '', location: 'Hamura', note: 'as Los Amidas/with ショルヘーノ, すずきかのん, 多賀文音' }
 ]
 
 const Page = () => {
@@ -118,10 +118,11 @@ const Page = () => {
               const n = performances.length - idx
               const dateStr = format(parse(p.date, 'yyyyMMdd', new Date), 'MMM d, yyyy')
               const dateLink = <Link href={`/diary/entry/${p.date}`} className="text-blue-600">{dateStr}</Link>
+              const note = p.location && p.note ? `${p.location}, ${p.note}` : p.location || p.location || ''
               if (!p.title)
-                return <p className="py-1" key={idx}>{n}. {dateLink} ({p.note}).</p>
+                return <p className="py-1" key={idx}>{n}. {dateLink} ({note}).</p>
               else {
-                return <p className="py-1" key={idx}>{n}. {dateLink}, <span className="italic">{p.title}</span>{p.note ? ` (${p.note})` : ''}.</p>
+                return <p className="py-1" key={idx}>{n}. {dateLink}, <span className="italic">{p.title}</span> ({note}).</p>
               }
             })}
           </div>
