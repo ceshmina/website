8324388e8829edc7dbd87b10e8e36073827ac9d3
ceshmina <shu@ceshmina.com>
2024-06-07 21:43:30 +0900

    最初と最後のページへのリンクを追加

--- components/diary/pagination.tsx
+++ components/diary/pagination.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
+import { ChevronLeftIcon, ChevronDoubleLeftIcon, ChevronRightIcon, ChevronDoubleRightIcon } from '@heroicons/react/24/solid'
 import { Paginator } from '@/core/diary/pagination'
 
 const Pagination = (props: { paginator: Paginator, page: number }) => {
@@ -18,18 +18,32 @@ const Pagination = (props: { paginator: Paginator, page: number }) => {
       <p className="py-1">
         {page > 1
           ? <span className="mr-2 border-[1px] border-gray-300 px-1 py-1 rounded-full">
+              <Link href={`/diary/list/1`} className="text-blue-500"><ChevronDoubleLeftIcon className="w-4 h-4 inline-block pb-0.5" /></Link>
+            </span>
+          : <span className="mr-2 border-[1px] border-gray-200 px-1 py-1 rounded-full text-gray-300">
+              <ChevronDoubleLeftIcon className="w-4 h-4 inline-block pb-0.5" />
+            </span>}
+        {page > 1
+          ? <span className="mr-2 border-[1px] border-gray-300 px-1 py-1 rounded-full">
               <Link href={`/diary/list/${page - 1}`} className="text-blue-500"><ChevronLeftIcon className="w-4 h-4 inline-block pb-0.5" /></Link>
             </span>
           : <span className="mr-2 border-[1px] border-gray-200 px-1 py-1 rounded-full text-gray-300">
               <ChevronLeftIcon className="w-4 h-4 inline-block pb-0.5" />
             </span>}
         {` ${page} / ${numPages} `}
-        {page < Math.ceil(n / perPage)
+        {page < numPages
           ? <span className="ml-2 border-[1px] border-gray-300 px-1 py-1 rounded-full">
               <Link href={`/diary/list/${page + 1}`} className="text-blue-500"><ChevronRightIcon className="w-4 h-4 inline-block pb-0.5" /></Link>
             </span>
           : <span className="ml-2 border-[1px] border-gray-200 px-1 py-1 rounded-full text-gray-300">
               <ChevronRightIcon className="w-4 h-4 inline-block pb-0.5" />
+            </span>}
+        {page < numPages
+          ? <span className="ml-2 border-[1px] border-gray-300 px-1 py-1 rounded-full">
+              <Link href={`/diary/list/${numPages}`} className="text-blue-500"><ChevronDoubleRightIcon className="w-4 h-4 inline-block pb-0.5" /></Link>
+            </span>
+          : <span className="ml-2 border-[1px] border-gray-200 px-1 py-1 rounded-full text-gray-300">
+              <ChevronDoubleRightIcon className="w-4 h-4 inline-block pb-0.5" />
             </span>}
       </p>
     </div>
