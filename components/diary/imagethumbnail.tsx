90b7513d1678cee1d454f673ed3138cab936decb
ceshmina <shu@ceshmina.com>
2024-06-08 16:53:00 +0900

    画像が中央に配置されるように

--- components/diary/imagethumbnail.tsx
+++ components/diary/imagethumbnail.tsx
@@ -13,7 +13,7 @@ const ImageThumbnail = (props: { src: string, largeSrc: string, diaryLink: strin
   return (<div>
     <img onClick={toggleZoom} src={src} loading="lazy" />
     {isZoom && <div onClick={toggleZoom} className="fixed inset-0 w-[100%] h-[100%] bg-black bg-opacity-80">
-      <div className="absolute top-[50%] left-[50%] w-[100%] h-[100%] translate-x-[-50%] translate-y-[-50%]">
+      <div className="absolute top-[50%] left-[50%] w-[100%] h-[100%] translate-x-[-50%] translate-y-[-50%] flex justify-center">
         <img src={largeSrc} loading="lazy" className="mx-auto my-auto max-w-[100%] max-h-[100%]" />
       </div>
     </div>}
