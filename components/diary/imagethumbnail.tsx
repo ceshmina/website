89acba3f1a603e4ff532653a86a6f432d10ab04e
ceshmina <shu@ceshmina.com>
2024-06-08 16:49:51 +0900

    写真一覧ページの拡大機能を差し替え

--- components/diary/imagethumbnail.tsx
+++ components/diary/imagethumbnail.tsx
@@ -1,69 +1,22 @@
 'use client'
-import { ReactElement, useMemo } from 'react'
-import { useState, useLayoutEffect } from 'react'
-import Zoom from 'react-medium-image-zoom'
-
-const cx = (mods: Record<string, boolean>): string => {
-  const cns: string[] = []
-  for (const k in mods) {
-    if (mods[k]) {
-      cns.push(k)
-    }
-  }
-  return cns.join(' ')
-}
+import { useState } from 'react'
 
 const ImageThumbnail = (props: { src: string, largeSrc: string, diaryLink: string, diaryTitle: string }) => {
   const { src, largeSrc, diaryLink, diaryTitle } = props
   if (!src) return null
 
-  const ZoomContent = ({ buttonUnzoom, modalState, img }: {
-    buttonUnzoom: React.ReactElement<HTMLButtonElement>,
-    modalState: string,  // ModalState
-    img: React.ReactElement | null
-  }) => {
-    const [isLoaded, setIsLoaded] = useState(false)
-
-    const imgProps = (img as ReactElement<HTMLImageElement>)?.props
-    const imgWidth = imgProps?.width
-    const imgHeight = imgProps?.height
-
-    const classCaption = useMemo(() => {
-      const hasWidthHeight = imgWidth != null && imgHeight != null
-      const imgRatioLargerThanWindow =
-        imgWidth / imgHeight > window.innerWidth / window.innerHeight
-
-      return cx({
-        'zoom-caption': true,
-        'zoom-caption--loaded': isLoaded,
-        'zoom-caption--bottom': hasWidthHeight && imgRatioLargerThanWindow,
-        'zoom-caption--left': hasWidthHeight && !imgRatioLargerThanWindow,
-      })
-    }, [imgWidth, imgHeight, isLoaded])
-
-    useLayoutEffect(() => {
-      if (modalState === 'LOADED') {
-        setIsLoaded(true)
-      } else if (modalState === 'UNLOADING') {
-        setIsLoaded(false)
-      }
-    }, [modalState])
-
-    return <div>
-      {buttonUnzoom}
-      <figure>
-        {img}
-        <figcaption className={classCaption}>
-          <a href={diaryLink} className='text-blue-500'>{diaryTitle}</a>
-        </figcaption>
-      </figure>
-    </div>
+  const [isZoom, setIsZoom] = useState(false)
+  const toggleZoom = () => {
+    setIsZoom(!isZoom)
   }
 
   return (<div>
-    <Zoom ZoomContent={ZoomContent} zoomImg={{ alt: '', src: largeSrc }}>
-      <img src={src} loading='lazy' />
-    </Zoom>
+    <img onClick={toggleZoom} src={src} loading="lazy" />
+    {isZoom && <div onClick={toggleZoom} className="fixed inset-0 w-[100%] h-[100%] bg-black bg-opacity-80">
+      <div className="absolute top-[50%] left-[50%] w-[100%] h-[100%] translate-x-[-50%] translate-y-[-50%]">
+        <img src={largeSrc} loading="lazy" className="mx-auto my-auto max-w-[100%] max-h-[100%]" />
+      </div>
+    </div>}
   </div>)
 }
 
