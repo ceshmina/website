26f18c586ee3a7df28b5340317ca450be3e02e1b
ceshmina <shu@ceshmina.com>
2024-06-08 17:02:02 +0900

    拡大画像から日記へのリンク追加

--- components/diary/imagethumbnail.tsx
+++ components/diary/imagethumbnail.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState } from 'react'
+import Link from 'next/link'
 
 const ImageThumbnail = (props: { src: string, largeSrc: string, diaryLink: string, diaryTitle: string }) => {
   const { src, largeSrc, diaryLink, diaryTitle } = props
@@ -15,6 +16,9 @@ const ImageThumbnail = (props: { src: string, largeSrc: string, diaryLink: strin
     {isZoom && <div onClick={toggleZoom} className="fixed inset-0 w-[100%] h-[100%] bg-black bg-opacity-80">
       <div className="absolute top-[50%] left-[50%] w-[100%] h-[100%] translate-x-[-50%] translate-y-[-50%] flex justify-center">
         <img src={largeSrc} loading="lazy" className="mx-auto my-auto max-w-[100%] max-h-[100%]" />
+        <figcaption className="zoom-caption zoom-caption--loaded zoom-caption--bottom">
+          <Link href={diaryLink} className="text-blue-500">{diaryTitle}</Link>より
+        </figcaption>
       </div>
     </div>}
   </div>)
