84c2a1c4917e457743b988cc1d6744368113463b
ceshmina <shu@ceshmina.com>
2024-06-24 14:33:18 +0900

    リファクタ: 日記取得周りの共通化など

--- core/model/diary.ts
+++ core/model/diary.ts
@@ -1,7 +1,16 @@
 import { Collection } from '@/core/model/base'
 import { PhotoUrl, PhotoCollection } from '@/core/model/photo'
-import { fetchDiaries, fetchDiaryBySlug } from '@/core/retrieve/diary'
+import { type FetchDiaryResponse, fetchDiaries, fetchDiaryBySlug } from '@/core/retrieve/diary'
 import { extractPhotoUrls } from '@/core/util/markdown'
+
+const fetchResponseToDiary = async (res: FetchDiaryResponse): Promise<Diary> => {
+  const { slug, metadata, content } = res
+  const title = metadata.title || null
+  const location = metadata.location || null
+  const photoUrls = extractPhotoUrls(content).map(url => new PhotoUrl(url))
+  const photos = await PhotoCollection.fetchByUrls(photoUrls)
+  return new Diary(slug, content, title, location, photos)
+}
 
 class Diary {
   private _slug: string
@@ -20,20 +29,19 @@ class Diary {
 
   static async fetchBySlug(slug: string): Promise<Diary | null> {
     const res = await fetchDiaryBySlug(slug)
-    if (!res) return null
-    const photoUrls = extractPhotoUrls(res.content).map(url => new PhotoUrl(url))
-    const photos = await PhotoCollection.fetchByUrls(photoUrls)
-    return new Diary(slug, res.title, res.location, res.content, photos)
+    if (res) {
+      return await fetchResponseToDiary(res)
+    } else {
+      return null
+    }
   }
 }
 
 class DiaryCollection extends Collection<Diary> {
   static async fetch(): Promise<DiaryCollection> {
-    const diaries = await fetchDiaries()
-    return new DiaryCollection(await Promise.all(diaries.map(async diary => {
-      const photoUrls = extractPhotoUrls(diary.content).map(url => new PhotoUrl(url))
-      const photos = await PhotoCollection.fetchByUrls(photoUrls)
-      return new Diary(diary.slug, diary.title, diary.location, diary.content, photos)
-    })))
+    const ress = await fetchDiaries()
+    return new DiaryCollection(await Promise.all(
+      ress.map(async res => await fetchResponseToDiary(res))
+    ))
   }
 }
--- core/retrieve/diary.ts
+++ core/retrieve/diary.ts
@@ -4,7 +4,24 @@ import matter from 'gray-matter'
 
 const DIARY_DIR = 'data/diary'
 
-export const fetchDiaries = cache(async () => {
+export type FetchDiaryResponse = {
+  slug: string
+  metadata: { [key: string]: any }
+  content: string
+}
+
+const markdownPathToDiary = async (path: string): Promise<FetchDiaryResponse> => {
+  const slug = path.split('/').pop()!.replace('.md', '')
+  const markdown = await fs.readFile(path, 'utf-8')
+  const { data, content } = matter(markdown)
+  return {
+    slug,
+    metadata: data,
+    content
+  }
+}
+
+export const fetchDiaries: () => Promise<FetchDiaryResponse[]> = cache(async () => {
   const _search = async (dir: string): Promise<string[]> => {
     const entries = await fs.readdir(dir, { withFileTypes: true })
     const diaries: string[] = []
@@ -19,20 +36,10 @@ export const fetchDiaries = cache(async () => {
     return diaries
   }
   const paths = await _search(DIARY_DIR)
-  return await Promise.all(paths.map(async path => {
-    const slug = path.split('/').pop()!.replace('.md', '')
-    const markdown = await fs.readFile(path, 'utf-8')
-    const { data, content } = matter(markdown)
-    return {
-      slug,
-      content,
-      title: data.title || null,
-      location: data.location || null
-    }
-  }))
+  return await Promise.all(paths.map(async path => await markdownPathToDiary(path)))
 })
 
-export const fetchDiaryBySlug = cache(async (slug: string) => {
+export const fetchDiaryBySlug: (slug: string) => Promise<FetchDiaryResponse | null> = cache(async (slug: string) => {
   const _search = async (dir: string, slug: string): Promise<string | null> => {
     const entries = await fs.readdir(dir, { withFileTypes: true })
     for (const entry of entries) {
@@ -46,14 +53,9 @@ export const fetchDiaryBySlug = cache(async (slug: string) => {
     return null
   }
   const path = await _search(DIARY_DIR, slug)
-  if (!path) return null
-  
-  const markdown = await fs.readFile(path, 'utf-8')
-  const { data, content } = matter(markdown)
-  return {
-    slug,
-    content,
-    title: data.title || null,
-    location: data.location || null
+  if (path) {
+    return await markdownPathToDiary(path)
+  } else {
+    return null
   }
 })
