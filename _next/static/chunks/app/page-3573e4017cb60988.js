dd1321321664cdb50c83905c7edd06adea8e6710
ceshmina <ceshmina@users.noreply.github.com>
2024-06-28 13:40:02 +0000

    deploy: e494d03293e411a044911dc6973dc60b9612bc31

---
+++ _next/static/chunks/app/page-3573e4017cb60988.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,832,453,68,797,370,437],{1365:function(e,n,s){Promise.resolve().then(s.t.bind(s,5250,23)),Promise.resolve().then(s.t.bind(s,4103,23))}},function(e){e.O(0,[112,971,69,744],function(){return e(e.s=1365)}),_N_E=e.O()}]);
