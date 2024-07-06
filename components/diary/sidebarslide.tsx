c6fb14ec33af81b8a48f33033f91d65b3a31d4df
ceshmina <shu@ceshmina.com>
2024-07-06 15:17:38 +0900

    サイドバークリックで閉じる

--- components/diary/sidebarslide.tsx
+++ components/diary/sidebarslide.tsx
@@ -1,10 +1,14 @@
 'use client'
+import { useState } from 'react'
 import { slide as Menu } from 'react-burger-menu'
 
-const SidebarSlide = ({ children }: { children: React.ReactNode }) => {
+const SidebarSlide = ({ children }: { children: React.ReactNode }) => { 
+  const [isOpen, setOpen] = useState(false)
+  const open = () => { setOpen(true) }
+  const close = () => { setOpen(false) }
   return (
-    <Menu right>
-      <div className="pb-32">{children}</div>
+    <Menu right isOpen={isOpen} onOpen={open} onClose={close}>
+      <div className="pb-32" onClick={close}>{children}</div>
     </Menu>
   )
 }
