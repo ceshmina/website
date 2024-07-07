73f575891f800610c4347b0a4fb9e5d52f9d5de7
ceshmina <shu@ceshmina.com>
2024-07-07 18:09:30 +0900

    フッターのリファクタ

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,59 +1,38 @@
-import type { Metadata } from 'next'
-import Link from 'next/link'
-import { Bitter, Noto_Sans_JP, Sono } from 'next/font/google'
-import { EN_TITLE_FONT } from '@/config'
-import { FaXTwitter, FaInstagram, FaGithub } from 'react-icons/fa6'
-import { PiNotebookFill } from 'react-icons/pi'
-import { MdHome } from 'react-icons/md'
+import { Bitter, Noto_Sans_JP, Josefin_Slab, Sono } from 'next/font/google'
 
-import './zoom.css'
+import { Footer } from '@/components/common/footer'
+
 import './burger.css'
-import './globals.css'
 import '/node_modules/flag-icons/css/flag-icons.min.css'
+import './zoom.css'
+import './globals.css'
 
-const mainFont = Bitter({ subsets: ['latin'], weight: ["300", "500"], variable: '--font-main' })
-const mainJPFont = Noto_Sans_JP({ subsets: ['latin'], weight: ["300", "500"], variable: '--font-jp' })
-const monoFont = Sono({ subsets: ['latin'], weight: ["300"], variable: '--font-code' })
 
-export const metadata: Metadata = {
-  title: 'ceshmina'
+const mainFont = Bitter({ subsets: ['latin'], weight: ['300', '500'], variable: '--font-main' })
+const mainJPFont = Noto_Sans_JP({ subsets: ['latin'], weight: ['300', '500'], variable: '--font-jp' })
+const altFont = Josefin_Slab({ subsets: ['latin'], weight: ['500', '700'], variable: '--font-alt' })
+const monoFont = Sono({ subsets: ['latin'], weight: ['300'], variable: '--font-code' })
+
+
+export const metadata = {
+  title: 'apkas'
 }
 
-const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+
+const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <html lang="en">
-      <body className={`${mainFont.variable} ${mainJPFont.variable} ${monoFont.variable} ${EN_TITLE_FONT.variable}`}>{children}</body>
+    <html lang="ja">
+      <body className={`${mainFont.variable} ${mainJPFont.variable} ${altFont.variable} ${monoFont.variable}`}>
+        {children}
 
-      <footer className={`${mainFont.className} font-light mt-12 pt-8 pb-2 text-gray-100 bg-gray-800`}>
-        <div className="max-w-[800px] mx-auto px-4 md:px-1 flex">
-          <div className="py-1 md:ml-[-4px] text-xs pr-8 md:pr-4 border-r-[1px] border-gray-300">
-            <p className="py-0.5 md:inline">
-              <FaXTwitter className="inline pb-[1px]" /> <a href="https://x.com/ceshmina" target="_blank" className="text-blue-300">ceshmina</a>
-            </p>
-            <p className="py-0.5 md:inline md:pl-4">
-              <FaInstagram className="inline pb-[1px]" /> <a href="https://instagram.com/ceshmina" target="_blank" className="text-blue-300">ceshmina</a>
-            </p>
-            <p className="py-0.5 md:inline md:pl-4">
-              <FaGithub className="inline pb-[1px]" /> <a href="https://github.com/ceshmina" target="_blank" className="text-blue-300">ceshmina</a>
-            </p>
-          </div>
-
-          <div className="py-1 pl-8 md:pl-4 text-xs">
-            <p className="py-0.5 md:inline">
-              <MdHome className="inline pb-[1.5px] text-sm" /> <Link href="/" className="text-blue-300">apkas.net</Link>
-            </p>  
-            <p className="py-0.5 md:inline md:pl-4">
-              <PiNotebookFill className="inline pb-[1.5px] text-sm" /> <Link href="/diary" className="text-blue-300">diary</Link>
-            </p>
+        <div className="mt-12 bg-gray-800 py-8">
+          <div className="max-w-[800px] mx-auto px-4 md:px-0 text-gray-100">
+            <Footer />
           </div>
         </div>
-
-        <div className="max-w-[800px] px-4 md:px-0 pt-4 pb-8 mx-auto">
-          <p className="text-[10px] text-gray-300">&copy;2024 APKAS</p>
-        </div>
-      </footer>
+      </body>
     </html>
   )
 }
 
-export default RootLayout
+export default Layout
---
+++ components/common/footer.tsx
@@ -0,0 +1,61 @@
+import Link from 'next/link'
+
+import { IconType } from 'react-icons'
+import { MdHome } from 'react-icons/md'
+import { PiNotebookFill } from 'react-icons/pi'
+import { FaXTwitter, FaInstagram, FaGithub } from 'react-icons/fa6'
+
+
+const InnerLink = ({ href, icon, children }: Readonly<{ href: string, icon: IconType, children: React.ReactNode }>) => {
+  const Icon = icon
+  return (
+    <span>
+      <Icon className="text-sm inline-block mr-1" />
+      <Link href={href} className="text-blue-300 hover:text-blue-500 transition">{children}</Link>
+    </span> 
+  )
+}
+
+const OuterLink = ({ href, icon, children }: Readonly<{ href: string, icon: IconType, children: React.ReactNode }>) => {
+  const Icon = icon
+  return (
+    <span>
+      <Icon className="text-xs inline-block mr-1" />
+      <Link href={href} target="_blank" className="text-blue-300 hover:text-blue-500 transition">{children}</Link>
+    </span> 
+  )
+}
+
+
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="text-xs flex">
+        <div>
+          <p className="my-2 md:my-1 md:inline-block">
+            <InnerLink href="/" icon={MdHome}>apkas.net</InnerLink>
+          </p>
+          <p className="my-2 md:my-1 md:inline-block md:ml-4">
+            <InnerLink href="/diary" icon={PiNotebookFill}>diary</InnerLink>
+          </p>
+        </div>
+
+        <div className="ml-8 md:ml-6 border-l-[1px] border-gray-300 pl-8 md:pl-6">
+          <p className="my-2 md:my-1 md:inline-block">
+            <OuterLink href="https://x.com/ceshmina" icon={FaXTwitter}>ceshmina</OuterLink>
+          </p>
+          <p className="my-2 md:my-1 md:inline-block md:ml-4">
+            <OuterLink href="https://instagram.com/ceshmina" icon={FaInstagram}>ceshmina</OuterLink>
+          </p>
+          <p className="my-2 md:my-1 md:inline-block md:ml-4">
+            <OuterLink href="https://github.com/ceshmina" icon={FaGithub}>ceshmina</OuterLink>
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <p className="text-[10px] text-gray-400">&copy;2024 APKAS</p>
+      </div>
+    </footer>
+  )
+}
