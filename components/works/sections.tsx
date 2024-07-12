e5146880aedbeaf4ac5822686d3c0111d8359dff
ceshmina <shu@ceshmina.com>
2024-07-12 20:48:44 +0900

    アルバムの並び順

--- components/works/sections.tsx
+++ components/works/sections.tsx
@@ -6,26 +6,6 @@ import { type AlbumProps, Album } from './album'
 
 const albums: AlbumProps[] = [
   {
-    title: 'Ambivalencia',
-    year: '2022',
-    descriptionJa: `
-西田フレンズとしての2ndアルバム。オンライン・コスキンで初披露した「Espiral」、リーダー・西田のボリビアへの想いを込めた「Para Ti Lejos」など、全11曲を収録。
-
-11曲中6曲を作曲。サンポーニャとギターを中心に演奏した他、全編のレコーディング・ミキシング・マスタリングを担当した。
-`,
-    descriptionEn: `
-The second album by Nishida Friends. It includes 11 tracks, such as "Espiral," which was first performed at the Online Cosquín, and "Para Ti Lejos," a song that reflects the leader Nishida's feelings towards Bolivia.
-
-Out of the 11 tracks, I composed 6. In addition to playing the zampoñas and guitar, I was responsible for the recording, mixing, and mastering of the entire album.
-`,
-    img: '/ambivalencia.webp',
-    links: [
-      { title: 'Spotify', icon: FaSpotify, url: 'https://open.spotify.com/intl-ja/album/1s3ObF3GkmBId0ZbXMkro4' },
-      { title: 'Apple Music', icon: SiApplemusic, url: 'https://music.apple.com/jp/album/ambivalencia/1658982598' },
-      { title: 'Amazon Music', icon: SiAmazonmusic, url: 'https://www.amazon.co.jp/music/player/albums/B0BPM6RZ5R?&_encoding=UTF8&tag=tcjaz-22&linkCode=ur2&camp=247&creative=1211' }
-    ]
-  },
-  {
     title: 'Aventura',
     year: '2021',
     descriptionJa: `
@@ -44,6 +24,26 @@ I composed 3 of the tracks and primarily played the zampoñas and guitar through
       { title: 'Apple Music', icon: SiApplemusic, url: 'https://music.apple.com/jp/album/aventura/1553005052' },
       { title: 'Amazon Music', icon: SiAmazonmusic, url: 'https://www.amazon.co.jp/music/player/albums/B08WC2Z716?&_encoding=UTF8&tag=tcjaz-22&linkCode=ur2&camp=247&creative=1211' }
     ]
+  },
+  {
+    title: 'Ambivalencia',
+    year: '2022',
+    descriptionJa: `
+西田フレンズとしての2ndアルバム。オンライン・コスキンで初披露した「Espiral」、リーダー・西田のボリビアへの想いを込めた「Para Ti Lejos」など、全11曲を収録。
+
+11曲中6曲を作曲。サンポーニャとギターを中心に演奏した他、全編のレコーディング・ミキシング・マスタリングを担当した。
+`,
+    descriptionEn: `
+The second album by Nishida Friends. It includes 11 tracks, such as "Espiral," which was first performed at the Online Cosquín, and "Para Ti Lejos," a song that reflects the leader Nishida's feelings towards Bolivia.
+
+Out of the 11 tracks, I composed 6. In addition to playing the zampoñas and guitar, I was responsible for the recording, mixing, and mastering of the entire album.
+`,
+    img: '/ambivalencia.webp',
+    links: [
+      { title: 'Spotify', icon: FaSpotify, url: 'https://open.spotify.com/intl-ja/album/1s3ObF3GkmBId0ZbXMkro4' },
+      { title: 'Apple Music', icon: SiApplemusic, url: 'https://music.apple.com/jp/album/ambivalencia/1658982598' },
+      { title: 'Amazon Music', icon: SiAmazonmusic, url: 'https://www.amazon.co.jp/music/player/albums/B0BPM6RZ5R?&_encoding=UTF8&tag=tcjaz-22&linkCode=ur2&camp=247&creative=1211' }
+    ]
   }
 ]
 
@@ -52,7 +52,7 @@ export const Works = (props: { lang: 'ja' | 'en' }) => {
   return (
     <section id="works">
       <h2 className="text-lg font-bold font-alt">WORKS</h2>
-      {albums.map((album, i) => (
+      {albums.slice().reverse().map((album, i) => (
         <Album key={i} {...album} lang={lang} />
       ))}
     </section>
