1b0717e33cfa01f51053c50f4bd67a7e73a43d6e
ceshmina <shu@apkas.net>
2024-08-03 23:00:43 +0900

    diary.apkas.netの記事も取り込む

--- scripts/clone.mjs
+++ scripts/clone.mjs
@@ -4,3 +4,7 @@ const git = simpleGit()
 const repoUrl = 'https://github.com/ceshmina/diary'
 const targetDir = 'data/diary'
 git.clone(repoUrl, targetDir)
+
+const repoUrl2 = 'https://github.com/ceshmina/diary.apkas.net'
+const targetDir2 = 'data/diary/2'
+git.clone(repoUrl2, targetDir2)
