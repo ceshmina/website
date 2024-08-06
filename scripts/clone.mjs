4bdf9e84fbaed16bc329444d513605c18e81776a
ceshmina <shu@apkas.net>
2024-08-06 22:35:50 +0900

    新レポジトリのreadmeを除外するため、日記のみコピーするように

--- scripts/clone.mjs
+++ scripts/clone.mjs
@@ -1,10 +1,13 @@
 import { simpleGit } from 'simple-git'
+import { copy } from 'fs-extra'
 
 const git = simpleGit()
 const repoUrl = 'https://github.com/ceshmina/diary'
 const targetDir = 'data/diary'
-git.clone(repoUrl, targetDir)
+await git.clone(repoUrl, targetDir)
 
 const repoUrl2 = 'https://github.com/ceshmina/diary.apkas.net'
-const targetDir2 = 'data/diary/2'
-git.clone(repoUrl2, targetDir2)
+const targetDir2 = 'data/diary2'
+await git.clone(repoUrl2, targetDir2)
+
+copy('data/diary2/_articles', 'data/diary/_articles')
